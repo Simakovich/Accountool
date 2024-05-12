a5c3f0ebeffd2f8e5d28a6b44da1d8df98260e36
BeRocker <ivansimakovich@gmail.com>
2024-05-12 22:19:36 +0300

    moved Indications to Indications

--- Accountool/Controllers/IndicationsController.cs
+++ Accountool/Controllers/IndicationsController.cs
@@ -10,6 +10,8 @@ using Accountool.Models.Entities;
 using Accountool.Models.Models;
 using System.Diagnostics.Metrics;
 using Accountool.Models.Services;
+using Accountool.Models.ViewModel;
+using Accountool.Utils;
 
 namespace Accountool.Controllers
 {
@@ -26,11 +28,102 @@ namespace Accountool.Controllers
             _measurementService = measurementService;
         }
 
-        // GET: Indications
-        public async Task<IActionResult> Index(int MeasureTypeId)
+        public async Task<IActionResult> Index()
         {
-            var datas = await _measurementService.GetFilteredMeasures(MeasureTypeId);
-            return View(datas);
+            var measurements = new MeasureWithIndication();
+            measurements.MeasureTypes = await _measurementService.GetAllMeasureTypes();
+            await FillMinMaxYear(measurements);
+            await FillMinMaxMonth(measurements);
+            return View("./../Measure/MasterPageMeasure", measurements);
+        }
+
+        public async Task<IActionResult> GetAllIndicationsByMeasure(
+            int id,
+            int? SelectedPlace,
+            int? SelectTown,
+            int? pageNumber,
+            int? SelectedStartYear = null,
+            int? SelectedLastYear = null,
+            int? SelectedStartMonth = null,
+            int? SelectedLastMonth = null)
+        {
+            var measureTypeId = id;
+            var measurements = new MeasureWithIndication();
+            measurements.MeasureTypes = await _measurementService.GetAllMeasureTypes();
+            measurements.TitleMeasureTypeName = measurements.MeasureTypes.FirstOrDefault(x => x.Id == measureTypeId)?.Name ?? string.Empty;
+            measurements.TitleMeasureTypeId = measureTypeId;
+            var indicationsQueriable = await _measurementService.GetFilteredMeasures(measureTypeId, SelectTown, SelectedPlace, SelectedStartMonth, SelectedLastMonth, SelectedStartYear, SelectedLastYear);
+            measurements.Indications = await PaginatedList<FullIndicationModel>.CreateAsync(indicationsQueriable, pageNumber ?? 1, Constants.PageSize);
+            await FillMinMaxYear(measurements);
+            await FillMinMaxMonth(measurements);
+            await FillPlaces(measurements);
+            await FillTowns(measurements);
+            return View("./../Measure/MasterPageMeasure", measurements);
+        }
+
+        private async Task FillMinMaxYear(MeasureWithIndication measurements)
+        {
+            if (measurements.TitleMeasureTypeId.HasValue)
+            {
+                var minMaxYear = await _measurementService.GetMinMaxYear(measurements.TitleMeasureTypeId.Value);
+                if (minMaxYear != null)
+                {
+                    measurements.MinYear = (int)((int?)minMaxYear.FirstDate.Year ?? Constants.FirstDayCurrentMonth.Date.Year);
+                    measurements.MaxYear = (int)((int?)minMaxYear.LastDate.Year ?? (int)Constants.LastDayCurrentMonth.Year);
+                    measurements.Years = Enumerable.Range(measurements.MinYear, measurements.MaxYear - measurements.MinYear + 1)
+                                        .Select(i => new SelectListItem
+                                        {
+                                            Value = i.ToString(),
+                                            Text = i.ToString()
+                                        }).ToList();
+                    measurements.SelectedLastYear = measurements?.Years?.Last()?.Value?.ToString();
+                }
+            }
+        }
+
+        private async Task FillMinMaxMonth(MeasureWithIndication measurements)
+        {
+            measurements.Months = Enumerable.Range(Constants.FirstMonth, Constants.LastMonth - Constants.FirstMonth + 1)
+                                .Select(i => new SelectListItem
+                                {
+                                    Value = i.ToString(),
+                                    Text = i.ToString()
+                                }).ToList();
+            measurements.SelectedLastMonth = Constants.LastMonth.ToString();
+        }
+
+        private async Task FillPlaces(MeasureWithIndication measurements)
+        {
+            if (measurements.TitleMeasureTypeId.HasValue)
+            {
+                var places = await _measurementService.GetPlaces(measurements.TitleMeasureTypeId.Value);
+                measurements.Places = places.Select(x => new { x.Id, x.Name })
+                                    .Distinct()
+                                    .Select(i => new SelectListItem
+                                    {
+                                        Value = i.Id.ToString(),
+                                        Text = i.Name.ToString()
+                                    }).ToList();
+                measurements.Places.Insert(0, new SelectListItem { Value = "", Text = "Please select" });
+                measurements.SelectedPlace = measurements?.Places?.FirstOrDefault()?.ToString();
+            }
+        }
+
+        private async Task FillTowns(MeasureWithIndication measurements)
+        {
+            if (measurements.TitleMeasureTypeId.HasValue)
+            {
+                var towns = await _measurementService.GetTowns(measurements.TitleMeasureTypeId.Value);
+                measurements.Towns = towns.Select(x => new { x.Id, x.Name })
+                                    .Distinct()
+                                    .Select(i => new SelectListItem
+                                    {
+                                        Value = i.Id.ToString(),
+                                        Text = i.Name.ToString()
+                                    }).ToList();
+                measurements.Towns.Insert(0, new SelectListItem { Value = "", Text = "Please select" });
+                measurements.SelectTown = measurements?.Towns?.FirstOrDefault()?.ToString();
+            }
         }
 
         // GET: Indications/Details/5
